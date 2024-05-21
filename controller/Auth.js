cccf6c2bf5b69402bb019108697915413e998d5f
MehraAnkit <110310312+MehraAnkit@users.noreply.github.com>
2024-05-21 16:02:30 +0530

    backend resolved

--- controller/Auth.js
+++ controller/Auth.js
@@ -79,7 +79,7 @@ exports.resetPasswordRequest = async (req, res) => {
 
     // Also set token in email
     const resetPageLink =
-      `https://mern-ecommerce-seven-gules.vercel.app/reset-password?token=` + token + '&email=' + email;
+      `https://mern-ecommerce-delta-pearl.vercel.app/reset-password?token=` + token + '&email=' + email;
     const subject = 'reset password for e-commerce';
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
